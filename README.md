cf6da5e40db86e80f68e73745faf9eacfb8eb61a
dujunhui <dujunhui@qq.com>
2016-12-19 16:47:45 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Tab-Control
-面向对象的选项卡切换
+# du_TabController
+
+-view：
+<div id="layOut-tab" class="tab">
+    <ul class="tab-nav clearFix">
+        <li class="active"><a href="javascript:;">新闻</a></li>
+        <li><a href="javascript:;">电影</a></li>
+        <li><a href="javascript:;">图片</a></li>
+    </ul>
+    <ul class="tab-con">
+        <li class="active"><div>点击事件：新闻的内容</div></li>
+        <li><div>电影的内容</div></li>
+        <li><div>图片的内容</div></li>
+    </ul>
+</div>
+
+-css:
+<link rel="stylesheet" href="css/reset.css"/>
+<link rel="stylesheet" href="css/du_TabController.css"/>
+
+
+-js:
+<script src="js/du_TabController.js"></script>
+
+-show1：
+var tab1 = new Tab('layOut-tab');
+tab1.init({});
+
+-show2：
+var tab2 = new Tab('layOut-tab2');
+tab2.init({
+    event:'mouseover'
+});
+
+-show3：
+var tab3 = new Tab('layOut-tab3');
+tab3.init({
+    event:'mouseover',
+    index:2,
+    delay:300
+});
+
+-show3：
+var tab4 = new Tab('layOut-tab4');
+tab4.init({
+    event:'mouseover',
+    delay:0
+});
+tab4.autoPlay(1000);
---
+++ js/du_TabController.js
@@ -0,0 +1,69 @@
+/**
+ * tab组件
+ */
+function Tab(id){
+    this.oParent = $('#'+id);//父级
+    this.aNav = this.oParent.find('.du_tabNav li');//tab标签
+    this.aCon = this.oParent.find('.du_tabCon li');//con标签
+    this.index = 0;
+    this.settings = {//默认参数
+        event : 'click',    //事件类型
+        index : 0,          //初始第几个
+        delay : 0           //时间
+    }
+}
+Tab.prototype.init = function(opt){
+    $.extend(this.settings,opt);
+    var This = this;
+    var delayTime = null;
+    //初始化第index个
+    this.index = this.settings.index;
+    this.show(this.aNav.eq(this.index).get(0));
+
+    //绑定事件
+    this.aNav.on(this.settings.event,function(){
+        var _this = this;
+        if(This.settings.event == 'mouseover' && This.settings.delay){
+            //有延迟的mouseover
+            delayTime = setTimeout(function(){
+                This.show(_this);
+            },This.settings.delay)
+        }else{
+            This.show(_this);
+        }
+
+    });
+    this.aNav.on('mouseout',function(){
+        clearTimeout(delayTime);
+    })
+
+}
+
+Tab.prototype.autoPlay = function(t){
+    var This = this;
+    var autoTime = null;
+    autoTime = setInterval(go,t);
+    this.aNav.on('mouseover',function(){
+        clearInterval(autoTime);
+    });
+    this.aNav.on('mouseout',function(){
+        autoTime = setInterval(go,t);
+    });
+    function go(){
+        var i = This.index+1;
+        i = i%(This.aNav.length);
+        //console.log(i);
+        This.show(This.aNav.eq(i).get(0));
+    }
+}
+
+Tab.prototype.show = function(obj){
+    //nav
+    this.aNav.removeClass('active');
+    this.aNav.eq($(obj).index()).addClass('active');
+    //con
+    this.aCon.removeClass('active');
+    this.aCon.eq($(obj).index()).addClass('active');
+    //改变当前的index下标
+    this.index = $(obj).index();
+}
